1bbdd925351ee9433187666f2fff389581a14f15
Mikeoo <mikeoverbeek@outlook.com>
2020-09-04 11:48:36 +0200

    added models

---
+++ Binder/Models/HouseProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Binder.Models
+{
+    public class HouseProfile
+    {
+        public string Id { get; set; }
+        public string City { get; set; }
+        public double Price { get; set; }
+        public HouseTypes HouseType { get; set; }
+    }
+}
---
+++ Binder/Models/HouseTypes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Binder.Models
+{
+    public enum HouseTypes
+    {
+        Bungalow = 1,
+        Apartment = 2,
+        Ranch = 3
+    }
+}
